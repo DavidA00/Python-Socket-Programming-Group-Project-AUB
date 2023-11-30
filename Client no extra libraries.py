51e1c241a3a02120a0177e4e24a22fb1b6918efc
dwa13 <dwa13@mail.aub.edu>
2023-12-01 01:30:49 +0200

    Added files

---
+++ Client no extra libraries.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 10:17:59 2023
+
+@author: mfa78
+"""
+
+import socket
+import sys
+
+#This is the client code with no extra libraries (Music and colors)
+buffer_size = 100000
+
+
+# Define variables
+HOST = socket.gethostname()  # Replace with the IP address of the server
+PORT = 8000  # Replace with the port number used by the server
+
+try:
+    # Set up socket and connect to server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+
+except ConnectionRefusedError: 
+    print("Connection to server failed!")
+    sys.exit(1)
+
+
+try:
+   
+    # Wait for welcome message from server
+    welcome_msg = sock.recv(buffer_size).decode()
+    print(welcome_msg)
+
+except (socket.error, ConnectionResetError) :
+    print("Error receiving welcome message from server:")
+    sys.exit(1)
+    
+
+print(sock.recv(buffer_size).decode()) 
+    
+
+
+
+# Start game loop
+
+for round_num in range(1,4):
+    # Receive random number from server and display it
+    try:
+        # will do a 3 second countdown 
+        print(sock.recv(buffer_size).decode())
+        print(sock.recv(buffer_size).decode())
+        print(sock.recv(buffer_size).decode())
+        
+        
+        rand_num = sock.recv(buffer_size).decode()
+
+
+
+        print(f"Round {round_num}: The random number is {rand_num} YALLA!!!!")
+        
+    except (socket.error,ConnectionResetError) :
+        print("Lost Connection with server")
+        sys.exit(1)
+
+       
+    
+
+    
+        
+        
+    # prompt the user for input
+
+    player_num = input("Enter the number as fast as possible: ")
+    if player_num != rand_num:
+        print("\nWRONG! YOU TYPED WRONG NUMBER!\n")
+    else:
+        print("Correct!")
+
+            
+    try: 
+        sock.send(player_num.encode())
+            
+    except :
+        print("The server has disconnected")
+        sys.exit(1)
+
+    
+    
+    
+    
+    # Results of the current round in descending order from server but i don't know if we need to display them
+    table = sock.recv(buffer_size).decode()
+    print(table)
+    
+    cumulative_table = sock.recv(buffer_size).decode()
+    inbet = sock.recv(buffer_size).decode()
+
+    print(cumulative_table)
+    print(inbet)
+
+
+
+
+
+
+# Receive final results and declare winner
+
+winner = sock.recv(buffer_size).decode()
+print(winner)
+
+
+
+# Close the connection
+sock.close()
---
+++ Server coloured and with music.py
@@ -0,0 +1,295 @@
+import socket
+import random
+import time
+import sys
+from math import ceil
+from termcolor import colored
+import pygame
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QLabel, QApplication
+import sys
+
+#Initiliaze pygram module for playing audio
+pygame.init()
+
+#load the game music
+pygame.mixer.music.load(r"C:\Users\ahmad\Desktop\350\Game sound 12 mins.mp3")
+pygame.mixer.music.play()
+
+#create a server socket
+server_socket = socket.socket()
+try:
+    #retrieve name of device on which server is set
+    hostname = socket.gethostname()
+        #we choose this port number
+    portnumber = 8000
+    #bind the server socket to a port
+    server_socket.bind((hostname, portnumber))
+
+except socket.error as e:
+    print("Error: ", str(e))
+    pygame.mixer.music.stop()
+    server_socket.close()
+    sys.exit(1)
+#chose number of players
+num_players = int(input("Please enter the number of players that will join this game: "))
+#listen for connections
+server_socket.listen(num_players)
+
+
+# Set up the lists to store the client connections and scores
+clients = []
+scores = [] #will contain the cumulative score for the player 
+print("Awaiting connections...")
+player_number= 0
+for i in range(num_players):
+    try:
+        #Accept connection from client
+        connection, address = server_socket.accept()
+        #get player number
+        player_number+=1
+        
+        welcome_msg= "Welcome, Player "+ str(player_number)+"\nYou have successfully connected to the game Server!"
+        connection.send(welcome_msg.encode())
+        
+        #add client connection to the list of clients
+        clients.append(connection)
+        #initialize score of player
+        scores.append([player_number,0])
+        #we add i+1 since players are numbered from 1 to num_players-1
+        print("Player " + str(player_number) + " has connected")
+        
+        
+    except socket.error as e:
+        print("Error in accepting the client: ", str(e))
+    
+   
+#message to indicate game is about to start and indicate error in case player has left
+for i in range(len(clients)):
+    try:
+        clients[i].send("THE GAME WILL START SHORTLY!".encode())
+    except ( ConnectionResetError) :
+         endofgame = ("Sorry!!!! \n Player" + str(i+1)+ " has left, the game has ended!" )
+         print("end of game")
+
+         for j in range(len(clients)):
+             if j!= (i-1):
+                 clients[j].send((endofgame).encode())
+         pygame.mixer.music.stop()
+         server_socket.close()        
+         sys.exit(1)
+         
+#wait for 3 seconds before starting the game
+time.sleep(3)
+
+# the connection is secure (TCP LIKE )
+# and handle errors 
+for round_num in range(1,4):
+    
+   
+    #this array will contain the round scores for all players  
+    #scores are stored as a list where (i= player number, j= score)
+    #if wrong number is pressed
+    #if error on any client,end game
+    
+
+    
+    round_scores = []
+    print("started round",round_num)
+    
+    
+    t= 1
+    #now, we insert a t second delay to give players time to be ready for next round
+    
+    worst_score_this_round=0  # just a counter that will be updated to the largest RTT this round
+    
+    # after player_number players connect, keep these players in the game 
+    #we generate random number of each player between 0 and 9
+    for i in range(1,player_number+1):
+        
+        random_num = random.randint(0, 9)
+        message=str(random_num) 
+        clients[i-1].settimeout(12)
+        try:
+        #send the random numbers to the client
+            clients[i-1].send("THREE".encode())
+            time.sleep(t)
+            clients[i-1].send("TWO".encode())
+            time.sleep(t)
+            clients[i-1].send("ONE".encode())
+            time.sleep(t)
+            print( " Sending random number to Player", i)
+            clients[i-1].send((message).encode())
+            print( " Sent random number to Player", i)
+
+            #print on client side 
+            start= time.time()
+            
+        except ( ConnectionResetError) :
+             endofgame = ("Error receiving data from player" + str(i)+ " the game has ended." )
+             winner = scores[0][0]
+             endofgame+= ("\nThe winner is Player " + str(winner)+ "!!!\n Good Game!")
+             print("end of game")
+             
+             for j in range(len(clients)):
+                 if j!= (i-1):
+                     clients[j].send((endofgame).encode())
+             pygame.mixer.music.stop()
+             server_socket.close()        
+             sys.exit(1)
+    
+        try:
+            recvd_num = clients[i-1].recv(1024).decode()
+            end= time.time()
+                    
+            try: 
+                recv = int(recvd_num)
+                if recv == random_num:
+                    score = end-start
+                    score= round(score,3)
+                    round_scores.append([i,score])
+                    if max(score, worst_score_this_round) == score:
+                        worst_score_this_round = score
+                    
+                else:
+                    round_scores.append([i,None])
+                
+            except ValueError: 
+                round_scores.append([i,None])
+                
+            
+    #Indicate error and end game in case player lost connection
+        except ConnectionResetError :
+        
+             endofgame = ("Error receiving data from player" + str(i)+ " the game has ended." )
+             winner = scores[0][0]
+             endofgame+= ("\nThe winner is Player " + str(winner)+ "!!!\n Good Game!")
+             print("end of game")
+             
+             for j in range(len(clients)):
+                 if j!= (i-1):
+                     clients[j].send((endofgame).encode())
+             pygame.mixer.music.stop()
+             server_socket.close()        
+             sys.exit(1)
+        #indicate timeout in case player took too much time to respond and end game
+        except( socket.timeout):
+             endofgame = (" Player " + str(i)+ " timed out! \n The game has ended" )
+             winner = scores[0][0]
+             endofgame+= ("\nThe winner is Player " + str(winner)+ "!!!\n Good Game!")
+             print("end of game")
+
+             for j in range(len(clients)):
+                 if j!= (i-1):
+                     clients[j].send((endofgame).encode())
+             pygame.mixer.music.stop()
+             server_socket.close()        
+             sys.exit(1)
+        
+    
+   #In case the player enters a wrong input he will get a score
+    for score in round_scores:
+        if score[1] == None: score[1] = ceil(worst_score_this_round)
+            
+
+    
+    # Update the scores list with the scores for the current round
+    
+    for score in scores:
+        client_number = score[0] 
+        for l in round_scores:
+            if l[0]== client_number:    
+                score[1] += l[1]
+                score[1]= round(score[1],3)
+                break
+        
+
+    # we are asked to display results in descending order; we will take this to mean : the best players on top, worst on bottom (in our case this is increasing order not descending)
+    round_scores.sort(reverse=False,key=lambda T: T[1])
+        
+    scores.sort(reverse=False,key=lambda T: T[1] )
+    
+    
+    table= ""
+    # Display the results for the current round in descending order(from lowest score/shortest time to top)
+    table+= ("\n Round " + str(round_num) + " scores:\n")
+
+        
+       
+    table+= ("| {:<8} | {:<6} | {:<0} |\n".format("PLAYER", "SCORE", "RANK"))
+    table+= ("|----------|--------|------|\n")
+    
+
+    
+    r=0
+    for s in round_scores :
+        r+=1
+        table+= ("| {:<7} | {:<6} | {:<2}   |\n".format("Player " + str(s[0]), str(s[1]), r))
+        
+    table=colored(table, "green")
+    
+    
+    
+    for i in range(len(clients)):
+        clients[i].send((table).encode())
+        
+        
+    cumulative_table= "" 
+    cumulative_table+= ("\n Cumulative Leaderboard:\n")
+    
+  
+    cumulative_table+=("| {:<8} | {:<6} | {:<0} |\n".format("PLAYER", "SCORE", "RANK"))
+    cumulative_table+=("|----------|--------|------|\n")
+    r=0
+    for s in scores :
+        r+=1
+        cumulative_table+=("| {:<7} | {:<6} | {:<2}   |\n".format("Player " + str(s[0]), str(s[1]), r))
+        
+        
+    cumulative_table = colored(cumulative_table, "red")
+
+    inbet= "________________________________________________\n"
+
+    if round_num !=3 :
+        inbet+="\n"
+        inbet+= "             GET READY FOR NEXT ROUND!"
+    else:
+        inbet+="\n"
+        inbet += "                     THE END\n"
+
+    inbet+= "________________________________________________\n"
+    inbet = colored(inbet, "cyan", attrs=["bold"])
+    
+    print( "sent results successfully for round",round_num )
+    for i in range(len(clients)):
+        clients[i].send((cumulative_table).encode())
+        clients[i].send(inbet.encode())
+
+    
+        
+#the winner is one with smallest score/ shortest time
+winner = scores[0][0]
+for client in clients:
+    client.send(("\nThe winner is Player " + str(winner)+ "!!!\n Good Game!").encode())
+
+#close my connections
+for client in clients:
+    client.close()
+
+pygame.mixer.music.stop()
+#close the server
+server_socket.close()
+
+# Create a Qt application
+app = QApplication(sys.argv)
+
+# Load the image
+img = QImage(r"C:\Users\ahmad\Desktop\350 (1)\350\Our Picture.jpg")
+
+# Create a label and set the image as the pixmap
+label = QLabel()
+pixmap = QPixmap.fromImage(img)
+label.setPixmap(pixmap)
+
+# Show the label inside Spyder
+label.show()
